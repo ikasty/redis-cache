9620f78e76b37b3dadaa2badc5417ac3a974b642
ikasty <mail.ikasty@gmail.com>
2024-02-25 10:42:17 +0900

    initial commit

---
+++ src/RedisCache.ts
@@ -0,0 +1,25 @@
+import { RedisClient } from './RedisClient';
+
+export class RedisCache {
+    private client: RedisClient;
+    private localCache: { [key: string]: string | null } = {};
+
+    constructor(client: RedisClient) {
+        this.client = client;
+    }
+
+    async write(key: string, value: string): Promise<void> {
+        await this.client.set(key, value);
+        this.localCache[key] = value;
+    }
+
+    async read(key: string): Promise<string | null> {
+        if (key in this.localCache) {
+            return this.localCache[key];
+        }
+
+        const value = await this.client.get(key);
+        this.localCache[key] = value;
+        return value;
+    }
+}
